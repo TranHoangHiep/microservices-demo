6ec1cd5749eae68e8c68fc40e8b28787894b1d93
hoang hiep <hiepth@mpos.vn>
2021-04-14 05:25:53 +0700

    exercise 13

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/KafkaAdminConfig.java
@@ -0,0 +1,22 @@
+package com.microservices.demo.kafka.admin.config;
+
+import com.microservices.demo.config.KafkaConfigData;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.Map;
+
+@EnableRetry
+@Configuration
+public class KafkaAdminConfig {
+
+    @Autowired
+    private KafkaConfigData kafkaConfigData;
+
+    public AdminClient adminClient() {
+        return AdminClient.create(Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers()));
+    }
+}
